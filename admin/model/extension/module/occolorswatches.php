e2758449177067e48681c3f2b5e778adddc12572
kazu7391 <vinhtq.0703@gmail.com>
2017-11-29 17:18:14 +0700

    alter table

--- admin/model/extension/module/occolorswatches.php
+++ admin/model/extension/module/occolorswatches.php
@@ -1,5 +1,27 @@
 <?php
 class ModelExtensionModuleOccolorswatches extends Model
 {
-    
+    public function installSwatchesAttribute() {
+        $check_sql = "SHOW COLUMNS FROM `" . DB_PREFIX . "product_image` LIKE 'option_value_id'";
+
+        $query = $this->db->query($check_sql);
+        if($query->rows) {
+            return;
+        } else {
+            $sql = "ALTER TABLE `" . DB_PREFIX . "product_image` ADD `option_value_id` INT(11) NULL";
+            $this->db->query($sql);
+            return;
+        }
+
+        $check_sql = "SHOW COLUMNS FROM `" . DB_PREFIX . "product` LIKE 'use_swatches'";
+
+        $query = $this->db->query($check_sql);
+        if($query->rows) {
+            return;
+        } else {
+            $sql = "ALTER TABLE `" . DB_PREFIX . "product` ADD `use_swatches` TINYINT(1) DEFAULT '0'";
+            $this->db->query($sql);
+            return;
+        }
+    }
 }
