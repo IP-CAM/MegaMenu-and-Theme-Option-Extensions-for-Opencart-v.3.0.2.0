fcc8ee4e12365b7b381701572d9ee03046278cf9
kazu7391 <vinhtq.0703@gmail.com>
2017-12-16 22:13:31 +0700

    Custom Grid Category Page 2

--- catalog/view/javascript/occategorygrid/grid.js
+++ catalog/view/javascript/occategorygrid/grid.js
@@ -1,1 +1,19 @@
-alert('a');
+$(document).ready(function () {
+    var type = $('#category-view-type').val();
+    var cols = $('#category-grid-cols').val();
+
+    category_view.initView(type, cols);
+});
+var category_view = {
+    'initView' : function (type, cols) {
+        category_view.changeView(type, cols);
+    },
+
+    'changeView' : function (type, cols) {
+        if(type == "grid") {
+        }
+
+        if(type == "list") {
+        }
+    }
+}
